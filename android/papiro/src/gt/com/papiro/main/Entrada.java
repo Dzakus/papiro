3f0651ccc44da1ffc95168935a2a9140f01a3698
Luis Cano <shreig@gmail.com>
2013-04-15 12:49:35 -0600

    Reversion de cambios de prueba

--- android/papiro/src/gt/com/papiro/main/Entrada.java
+++ android/papiro/src/gt/com/papiro/main/Entrada.java
@@ -3,6 +3,7 @@ package gt.com.papiro.main;
 import gt.com.papiro.interactivo.Exposicion;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Entrada extends Activity {
@@ -10,6 +11,9 @@ public class Entrada extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		Entrada.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
 		Intent intent = new Intent(this, Exposicion.class);		
 		startActivity(intent);
 	}
